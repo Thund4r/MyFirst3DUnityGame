716b5e097f33d56944dee8dae7f560cad0c3abfd
Thund4r <chongzhekhang@gmail.com>
2024-08-10 17:59:55 +0800

    Camera movement added

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -8,18 +8,25 @@ public class InputManager : MonoBehaviour
     private PlayerControls.GroundActions ground;
 
     private PlayerMotor playerMotor;
+    private PlayerLook playerLook;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerControls();
         ground = playerInput.Ground;
         playerMotor = GetComponent<PlayerMotor>();
+        playerLook = GetComponent<PlayerLook>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         playerMotor.ProcessMove(ground.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        playerLook.ProcessLook(ground.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
--- Assets/PlayerLook.cs
+++ Assets/PlayerLook.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera cam;
 
-    // Update is called once per frame
-    void Update()
+    private float yRotation = 0f;
+    private float xRotation = 0f;
+
+    public float ySensitivity = 10f;
+    public float xSensitivity = 10f;
+
+    public void ProcessLook(Vector2 input)
     {
-        
+        float mouseX = input.x;
+        float mouseY = input.y;
+        yRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        yRotation = Mathf.Clamp(yRotation, -80f, 80f);
+        xRotation = (mouseX * Time.deltaTime) * xSensitivity;
+        cam.transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+        transform.Rotate(0, xRotation, 0);
     }
 }
